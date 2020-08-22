96356f97a491f3ac3f5d6dc448f1a416e6aa2bff
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-08-22 10:12:58 +0700

    continued styling abit

---
+++ frontend-angular/src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+  username: string;
+  password: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  profile_pic: string;
+  cover_pic: string;
+  about_me: string;
+  total_hearts: number;
+  user_type: string;
+  role: string;
+}
